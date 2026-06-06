f0f32dddf120a8c6a9fe7d8a7ff59440d482f91f
Mehebub Sarkar <mehebubsarkar18@gmail.com>
2026-06-06 09:09:49 +0530

    feat: remove AI food image analysis backend logic

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -4,6 +4,5 @@ const aiController = require('../controllers/aiController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/chat', protect, aiController.chatWithAI);
-router.post('/analyze-food', protect, aiController.analyzeFoodImage);
 
 module.exports = router;
